2b53b625175a4646c3e50d31a3e73bf8641edf26
war-duck <01178893@pw.edu.pl>
2025-01-15 15:18:31 +0100

    add encrypted key field to db for every user

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from encrypted_model_fields.fields import EncryptedCharField
+from Crypto.PublicKey import RSA
 
+    
+def initialize_private_key():
+    return RSA.generate(2048).export_key('PEM')
+    
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=False, default='a@a.com')
-
+    email = models.EmailField(unique=False, default='a@a.com', blank=True)
+    private_key = EncryptedCharField(max_length=3000, default=initialize_private_key)
     def __str__(self):
         return self.username
